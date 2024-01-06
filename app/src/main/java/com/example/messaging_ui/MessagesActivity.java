1bfad5c1d93f6513fbb8ef1827696bb555687a22
Elle <cruz.244819@dasmarinas.sti.edu.ph>
2024-01-07 07:01:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/messaging_ui/MessagesActivity.java
@@ -0,0 +1,14 @@
+package com.example.messaging_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MessagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messaging_text_layout);
+    }
+}
